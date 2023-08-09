8ced83d9d257db27550c8154954d4cb0316cd5d8
Dmytro <Deynao1996@gmail.com>
2023-08-09 13:05:11 +0300

    case sensity fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import App from './components/App/App'
+import App from './components/app/App'
 
 const queryClient = new QueryClient()
 
